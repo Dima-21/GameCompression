b04a06c21c09ca9be42f9b4690f554806ea2a124
Dima <q>
2018-06-05 16:57:01 +0300

    1.0

--- Menu/Form1.Designer.cs
+++ Menu/Form1.Designer.cs
@@ -29,11 +29,173 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.speedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Speed1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.Speed2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.Speed3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.Speed4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.GameObject = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.gameToolStripMenuItem,
+            this.speedToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(343, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.stopToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.gameToolStripMenuItem.Text = "&Game";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.startToolStripMenuItem.Text = "St&art";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.stopToolStripMenuItem.Text = "Sto&p";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // speedToolStripMenuItem
+            // 
+            this.speedToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Speed1,
+            this.Speed2,
+            this.Speed3,
+            this.Speed4});
+            this.speedToolStripMenuItem.Name = "speedToolStripMenuItem";
+            this.speedToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.speedToolStripMenuItem.Text = "&Speed";
+            // 
+            // Speed1
+            // 
+            this.Speed1.Checked = true;
+            this.Speed1.CheckOnClick = true;
+            this.Speed1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.Speed1.Name = "Speed1";
+            this.Speed1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D1)));
+            this.Speed1.Size = new System.Drawing.Size(152, 22);
+            this.Speed1.Text = "1";
+            this.Speed1.Click += new System.EventHandler(this.Speed1_Click);
+            // 
+            // Speed2
+            // 
+            this.Speed2.Name = "Speed2";
+            this.Speed2.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D2)));
+            this.Speed2.Size = new System.Drawing.Size(152, 22);
+            this.Speed2.Text = "2";
+            this.Speed2.Click += new System.EventHandler(this.Speed2_Click);
+            // 
+            // Speed3
+            // 
+            this.Speed3.Name = "Speed3";
+            this.Speed3.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D3)));
+            this.Speed3.Size = new System.Drawing.Size(152, 22);
+            this.Speed3.Text = "3";
+            this.Speed3.Click += new System.EventHandler(this.Speed3_Click);
+            // 
+            // Speed4
+            // 
+            this.Speed4.Name = "Speed4";
+            this.Speed4.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D4)));
+            this.Speed4.Size = new System.Drawing.Size(152, 22);
+            this.Speed4.Text = "4";
+            this.Speed4.Click += new System.EventHandler(this.Speed4_Click);
+            // 
+            // GameObject
+            // 
+            this.GameObject.Location = new System.Drawing.Point(124, 151);
+            this.GameObject.Name = "GameObject";
+            this.GameObject.Size = new System.Drawing.Size(50, 50);
+            this.GameObject.TabIndex = 1;
+            this.GameObject.Text = "Hello";
+            this.GameObject.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 334);
+            this.Controls.Add(this.GameObject);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem speedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Speed1;
+        private System.Windows.Forms.ToolStripMenuItem Speed2;
+        private System.Windows.Forms.ToolStripMenuItem Speed3;
+        private System.Windows.Forms.ToolStripMenuItem Speed4;
+        private System.Windows.Forms.Button GameObject;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Menu/Form1.cs
+++ Menu/Form1.cs
@@ -12,9 +12,110 @@ namespace Menu
 {
     public partial class Form1 : Form
     {
+        enum LR { Left, Right };
+        LR side;
+        Size SizeGameObject;
+        bool isStart;
+        int speed;
         public Form1()
         {
             InitializeComponent();
+            speed = 2;
+            side = LR.Right;
+            isStart = false;
+            SizeGameObject = new Size(GameObject.Size.Width, GameObject.Size.Height);
+        }
+
+        private void Unchecked()
+        {
+            Speed1.Checked = Speed2.Checked = Speed3.Checked = Speed4.Checked = false;
+        }
+
+        private void Start()
+        {
+            isStart = true;
+            timer1.Start();
+        }
+
+        private bool Compression()
+        {
+            if (side == LR.Left && GameObject.Size.Width > 20)
+            {
+                GameObject.Size = new Size(GameObject.Size.Width - Math.Abs(speed), GameObject.Size.Height + Math.Abs(speed));
+                return false;
+            }
+            else if (side == LR.Right && GameObject.Size.Width > 20)
+            {
+                GameObject.Size = new Size(GameObject.Size.Width - Math.Abs(speed), GameObject.Size.Height + Math.Abs(speed));
+                return false;
+            }
+            else
+            {
+                GameObject.Size = new Size(GameObject.Size.Width + Math.Abs(speed), GameObject.Size.Height - Math.Abs(speed));
+                return true;
+            }
+        }
+        private void Move()
+        {
+            if (GameObject.Location.X <= 0 && Compression())
+            {
+                speed = Math.Abs(speed);
+                side = LR.Right;
+            }
+            else if (GameObject.Location.X >= this.ClientSize.Width - GameObject.Size.Width || speed > 0 && side == LR.Left )
+            {         
+                speed = -speed;
+                side = LR.Left;
+            }
+            GameObject.Location = new Point(GameObject.Location.X + speed, GameObject.Location.Y);
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+
+        private void Speed1_Click(object sender, EventArgs e)
+        {
+            Unchecked();
+            Speed1.Checked = true;
+            speed = 2;
+        }
+
+        private void Speed2_Click(object sender, EventArgs e)
+        {
+            Unchecked();
+            Speed2.Checked = true;
+            speed = 4;
+        }
+
+        private void Speed3_Click(object sender, EventArgs e)
+        {
+            Unchecked();
+            Speed3.Checked = true;
+            speed = 6;
+        }
+
+        private void Speed4_Click(object sender, EventArgs e)
+        {
+            Unchecked();
+                Speed4.Checked = true;
+            speed = 8;
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Move();
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
         }
     }
 }
